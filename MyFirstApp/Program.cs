d2ac706314f2ca5347d6b59edddff7c7ad4f9693
maksb <maksb@MAXIM-PC>
2022-06-24 12:40:13 +0700

    Добавьте файлы проекта.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money = 100;
+            string name = "Maxim";
+            bool isRealHuman = true;
+            float height = 1.84f;
+            sbyte mood = -100;
+            long metersToMoscow = 4151000;
+            byte floor = 5;
+            ulong loveToKittens = 18446744073709551615;
+            short humanPopulationMoon = 0;
+            uint humanMoscow = 12635466;
+            
+
+
+        }
+    }
+}
