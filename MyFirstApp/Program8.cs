20b8cf251c01fa6015a1e6e7be2b3d75f95f42a7
Maksibi <83632765+Maksibi@users.noreply.github.com>
2022-06-24 22:34:04 +0700

    Update Program8.cs

--- MyFirstApp/Program8.cs
+++ MyFirstApp/Program8.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,13 +12,15 @@ namespace MyFirstApp
         {
             int countOfRow;
             int minutesOfVisit = 10;
+            int minutesInHour = 60;
             Console.WriteLine("Введите количество бабусек в очереди.");
             countOfRow = Convert.ToInt32(Console.ReadLine());
 
             int minutesOfWait = minutesOfVisit * countOfRow;
-            int hoursOfWait = minutesOfWait / 60;
+            int hoursOfWait = minutesOfWait / minutesInHour;
+            int minutesOutHours = minutesOfWait - hoursOfWait * 60;
 
-            Console.WriteLine("Вы должны протерпеть в очереди {0}ч {1}м", hoursOfWait, minutesOfWait - hoursOfWait * 60 );
+            Console.WriteLine("Вы должны протерпеть в очереди {0}ч {1}м", hoursOfWait, minutesOutHours);
             Console.ReadKey();
         }
     }
