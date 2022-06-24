e961115103d4b42223a7d78cc167b08a58ae80b8
Maksibi <83632765+Maksibi@users.noreply.github.com>
2022-06-24 13:30:49 +0700

    Add files via upload

---
+++ MyFirstApp/Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ваш знак зодиака?");
+            string sign = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?");
+            var age = Console.ReadLine();
+            Console.WriteLine("Ваша профессия?");
+            string job = Console.ReadLine();
+            Console.WriteLine("Вас зовут {0}, вам {2} лет и вы {1}. Ваша профессия {3}.", name, sign, age, job);
+            Console.ReadKey();
+        }
+    }
+}
