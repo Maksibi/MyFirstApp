7db1fd455ca0a36d23f27d519ee7d4ffa5c3e16a
Maksibi <83632765+Maksibi@users.noreply.github.com>
2022-06-24 22:08:10 +0700

    Add files via upload

---
+++ MyFirstApp/Program7.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesInLine = 3;
+            int picturesInAlbum = 52;
+            int pictureLines = picturesInAlbum / picturesInLine;
+            int picturesOver = picturesInAlbum - pictureLines * picturesInLine;
+            Console.WriteLine("Полностью заполненных рядов картинок {0}, не поместилось картинок {1}", pictureLines, picturesOver);
+            Console.ReadKey();
+        }
+    }
+}
