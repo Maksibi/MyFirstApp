d7bb2bb70f5564abca5b1b6ef3d859d878275b74
Maksibi <83632765+Maksibi@users.noreply.github.com>
2022-06-24 21:00:28 +0700

    Add files via upload

---
+++ MyFirstApp/Program3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Bogdanov";
+            string surname = "Maxim";
+            Console.WriteLine("До: Name: {0}, Surname: {1}", name, surname);
+            (name, surname) = (surname, name);
+            Console.WriteLine("После: Name: {0}, Surname: {1}", name, surname);
+            Console.ReadKey();
+        }
+    }
+}
